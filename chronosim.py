e2dad7896cce8a8fdb3e91f01d89bb0b85750cd8
Jonathan Guiang <jkguiang@gmail.com>
2018-10-16 08:32:05 -0700

    Render function will plot hit LGADs, still debugging proximity detection

--- chronosim.py
+++ chronosim.py
@@ -4,7 +4,10 @@ from stl import mesh
 from mpl_toolkits import mplot3d
 from matplotlib import pyplot as plt
 
+from tqdm import tqdm
+
 def LoadMesh(stlPath):
+    """ Load polygon mesh, only return polygons with normal in z-direction """
     lgadMesh = mesh.Mesh.from_file(stlPath)
     allVectors = lgadMesh.vectors
     zVectors = []
@@ -12,13 +15,14 @@ def LoadMesh(stlPath):
         if (n[2] != 0):
             zVectors.append([ allVectors[i][0], allVectors[i][1], allVectors[i][2] ])
 
-    return np.array(zVectors)
+    return zVectors
 
 def LoadRays(rayPath):
+    """ Load ray trajectory data from txt file, map to dict """
     rays = []
     with open(rayPath, "r") as rayFile:
         for line in rayFile.readlines():
-            ray = [float(val) for val in line.split()]
+            ray = [ float(val) for val in line.split() ]
             rays.append({ "id":     ray[0],
                           "mass":   ray[1],
                           "charge": ray[2],
@@ -30,53 +34,54 @@ def LoadRays(rayPath):
 
     return rays
 
+def CheckHit(hitCand, ray):
+    """ Check that nearby ray's trajectory intersects polygon """
+
+    return True
+
 def ParseMesh(stlPath, rayPath):
+    """ Parse polygons and rays, look for hits """
     # Grab STL file
     polys = LoadMesh(stlPath)
     rays = LoadRays(rayPath)
+    hits = []
     # Loop over rays
-    for ray in rays:
+    for ray in tqdm(rays):
         isHit = False
+        rayHits = []
         # Loop over polygons
         for poly in polys:
-            isNear = False
-            # Check first vertex for proximity
+            # Calculate ray's origin proximity to first polygon vertex
             yDisp = abs(ray["pos"][1] - poly[0][1])
             xDisp = abs(ray["pos"][0] - poly[0][0])
-            if yDisp < 10 and xDisp < 10:
-                isNear = True
+            # Check for proximity and trajectory intersection
+            if xDisp < 10 and yDisp < 10 and CheckHit(poly, ray):
                 isHit = True
+                rayHits.append(poly)
                 print(ray["pos"], ray["dir"])
                 print(poly)
-                break # DEBUG
-        if not isHit: print("Fail")
-    return
 
-def render():
-    """ DEBUGGING: Render STL file """
-    # Grab STL file
-    lgadMesh = mesh.Mesh.from_file('stl/quad_lgad.stl')
+        if isHit:
+            hits.append({ "ray":ray, "hits":np.array(rayHits) })
 
-    # Define plot
-    plt.ion()
+    return hits
+
+def Render(hits):
+    """ Draw all hit polygons """
+
+    # Set up plot
     figure = plt.figure()
     axes = mplot3d.Axes3D(figure)
-    vectors = LoadMesh('stl/quad_lgad.stl')
-    print(vectors)
-    print("Finished with {} vectors".format(len(vectors)))
+
     # Auto scale to mesh size
-    scale = lgadMesh.points.flatten(-1)
-    axes.auto_scale_xyz(scale, scale, scale)
-    test = range(0,len(vectors))
-    test.reverse()
-    # Add vectors to plot
-    for i in test:
-        axes.add_collection3d(mplot3d.art3d.Poly3DCollection(vectors[i:len(vectors)]))
-        plt.show()
-        plt.pause(1)
+    # scale = lgadMesh.points.flatten(-1)
+    # axes.auto_scale_xyz(scale, scale, scale)
+
+    # Plot hits
+    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(hits["hits"]))
+    plt.show()
 
     return
 
 if __name__ == "__main__":
-    print("simulation")
     ParseMesh('stl/lgads.stl', 'txt/output_1927.txt')
