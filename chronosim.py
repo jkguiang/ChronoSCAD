c094fc80aa4ace636df670930250582389819efa
Jonathan Guiang <jkguiang@gmail.com>
2018-10-19 10:59:50 -0700

    now checks for intersections

--- chronosim.py
+++ chronosim.py
@@ -16,6 +16,7 @@ def LoadMesh(stlPath, sf=1, verbose=False):
             zVectors.append([ allVectors[i][0]*sf, allVectors[i][1]*sf, allVectors[i][2]*sf ])
 
     if (verbose):
+        print("From file: {}".format(stlPath))
         nLGAD = len(zVectors)/2
         nASIC = nLGAD*2
         print("---------------- Cost ----------------")
@@ -46,10 +47,21 @@ def LoadRays(rayPath):
 
     return rays
 
-def CheckHit(hitCand, ray):
-    """ Check that nearby ray's trajectory intersects polygon """
+def CrossProduct(a, b):
+    return (a[0]*b[1] - a[1]*b[0])
 
-    return True
+def CheckHit(poly, hitPos):
+    """ Check that nearby ray's trajectory intersects polygon """
+    hitPosToVertex = [[ (poly[0][0]-hitPos[0]), (poly[0][1]-hitPos[1]) ],
+                      [ (poly[1][0]-hitPos[0]), (poly[1][1]-hitPos[1]) ], 
+                      [ (poly[2][0]-hitPos[0]), (poly[2][1]-hitPos[1]) ]]
+
+    z0 = CrossProduct(hitPosToVertex[0], hitPosToVertex[1])
+    z1 = CrossProduct(hitPosToVertex[1], hitPosToVertex[2])
+    z2 = CrossProduct(hitPosToVertex[2], hitPosToVertex[0])
+
+    return (z0*z1*z2 < 0)
+
 
 def Parse(stlPath, rayPath):
     """ Parse polygons and rays, look for hits """
@@ -58,23 +70,30 @@ def Parse(stlPath, rayPath):
     rays = LoadRays(rayPath)
     hits = []
     # Loop over rays
-    for ray in tqdm(rays):
+    for ray in tqdm(rays[0:100]):
         isHit = False
         rayHits = []
+        # Calculate velocity of particle
+        vel = [ ray["dir"][0]/ray["mass"], 
+                ray["dir"][1]/ray["mass"], 
+                ray["dir"][2]/ray["mass"] ] 
         # Loop over polygons
         for poly in polys:
-            # Calculate ray's origin's proximity to first polygon vertex
-            xDisp = abs(ray["pos"][0] - poly[0][0])
-            yDisp = abs(ray["pos"][1] - poly[0][1])
+            # Calculate ray's proximity to first polygon vertex
+            t = abs(poly[0][2] - ray["pos"][2])/vel[2]
+            hitPos = [ ray["pos"][0]+t*vel[0], 
+                       ray["pos"][1]+t*vel[1],
+                       ray["pos"][2]+t*vel[2] ]
+            xDisp = abs(hitPos[0] - poly[0][0])
+            yDisp = abs(hitPos[1] - poly[0][1])
             # Check for proximity and trajectory intersection
-            if xDisp < 10 and yDisp < 10 and CheckHit(poly, ray):
+            if xDisp < 0.05 and yDisp < 0.05 and CheckHit(poly, hitPos):
                 isHit = True
                 rayHits.append(poly)
-                # print(ray["pos"], ray["dir"])
-                # print(poly)
 
         if isHit:
             hits.append({ "ray":ray, "hits":np.array(rayHits) })
+
     print(len(rays), len(hits), len(hits)/len(rays))
     return hits
 
@@ -112,21 +131,28 @@ def Debug(stlPath, txtPath):
 
     # Plot hits
     axes.add_collection3d(mplot3d.art3d.Poly3DCollection(vectors))
-#    for i in range(len(rays)):
-#        axes.plot3D(xs=rays[i][:,0], ys=rays[i][:,1], zs=rays[i][:,2], color=colors[i%nc])
-#
-#    sr, sl = 3.6, 15.0
-#    t = np.linspace(0, 2*np.pi, 100)
-#    axes.plot(xs=sr*np.cos(t), ys=sr*np.sin(t), zs=sl/2, color='k')
-#    axes.plot(xs=sr*np.cos(t), ys=sr*np.sin(t), zs=-sl/2, color='k')
-#    for i in range(8):
-#        th = i * 2*np.pi/8
-#        x = sr*np.cos(th)
-#        y = sr*np.sin(th)
-#        axes.plot(xs=[x,x], ys=[y,y], zs=[-sl/2, sl/2], color='k')
+    for i in range(len(rays)):
+        axes.plot3D(xs=rays[i][:,0], ys=rays[i][:,1], zs=rays[i][:,2], color=colors[i%nc])
+
+    sr, sl = 3.6, 15.0
+    t = np.linspace(0, 2*np.pi, 100)
+    axes.plot(xs=sr*np.cos(t), ys=sr*np.sin(t), zs=sl/2, color='k')
+    axes.plot(xs=sr*np.cos(t), ys=sr*np.sin(t), zs=-sl/2, color='k')
+    for i in range(8):
+        th = i * 2*np.pi/8
+        x = sr*np.cos(th)
+        y = sr*np.sin(th)
+        axes.plot(xs=[x,x], ys=[y,y], zs=[-sl/2, sl/2], color='k')
     plt.show()
 
     return
 
 if __name__ == "__main__":
-    LoadMesh(stlPath='stl/lgads.stl', sf=(0.001), verbose=True)
+    #LoadMesh(stlPath='stl/21mmElectronics.stl', sf=(0.001), verbose=True)
+    #Debug('stl/lgads.stl', 'txt/output_1927.txt')
+    #debugPoly = [ [0,0,0],
+    #              [0,10,0],
+    #              [10,0,0]]
+    #debugHit = [5,-0.00001,0]
+    #print(CheckHit(debugPoly, debugHit))
+    Parse('stl/lgads.stl', 'txt/output_1927.txt')
